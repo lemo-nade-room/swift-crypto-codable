6eb7838ebc2acee6a35661d7dd07fe2bc2b7e495
tanaka <lemo.nade.engineer.room@gmail.com>
2024-09-08 00:17:09 +0900

    Swift Package Init

---
+++ Tests/crypto-codableTests/crypto_codableTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import crypto_codable
+
+final class crypto_codableTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}

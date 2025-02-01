88b614398197a2c1c0f69cf05fe6ad754a08813e
tanaka <lemo.nade.engineer.room@gmail.com>
2025-02-01 13:39:06 +0900

    feat:️ ✨ 依存関係整理とSwift 6対応 - Swiftバージョンを5.10から6.0に更新 - `swift-testing`依存を削除 - StrictConcurrency設定を更新 - 一部インポートに@preconcurrencyを追加

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import Foundation
@@ -14,7 +14,6 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-testing.git", from: "0.10.0"),
         .package(url: "https://github.com/swiftlang/swift-docc-plugin.git", from: "1.0.0"),
         .package(url: "https://github.com/realm/SwiftLint.git", branch: "main"),
         .package(url: "https://github.com/apple/swift-crypto.git", from: "3.0.0"),
@@ -31,19 +30,18 @@ let package = Package(
         .testTarget(
             name: "CryptoCodableTests",
             dependencies: [
-                "CryptoCodable",
-                .product(name: "Testing", package: "swift-testing"),
+                "CryptoCodable"
             ],
             swiftSettings: swiftSettings,
             plugins: swiftLintPlugins
         ),
-    ]
+    ],
+    swiftLanguageModes: [.v6]
 )
 
 var swiftSettings: [SwiftSetting] {
     [
-        .enableUpcomingFeature("DisableOutwardActorInference"),
-        .enableExperimentalFeature("StrictConcurrency"),
+        .enableExperimentalFeature("StrictConcurrency")
     ]
 }
 
